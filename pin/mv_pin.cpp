70e6d98ccde0f63758b8cd7f3904164a6b953d4b
Andrew Clinton <ajclinto@gmail.com>
2013-11-24 21:30:10 -0500

    Add buffering and data type support.

--- pin/mv_pin.cpp
+++ pin/mv_pin.cpp
@@ -34,6 +34,15 @@
 // Global variables 
 /* ================================================================== */
 
+BUFFER_ID   theBuffer;
+
+struct BufferData {
+    ADDRINT     ea;
+    UINT64	type;
+};
+
+#define NUM_BUF_PAGES 4
+
 static MV_SharedData	*theSharedData = 0;
 static MV_TraceBlock	*theBlock = 0;
 static unsigned int	 theBlockIndex = 0;
@@ -101,34 +110,37 @@ flushEvents()
 
 PIN_LOCK    theLock;
 
-static inline void
-recordEvent(VOID *addr, unsigned long long type, UINT32 size, THREADID tid)
+/*!
+ * Called when a buffer fills up, or the thread exits, so we can process it or pass it off
+ * as we see fit.
+ * @param[in] id		buffer handle
+ * @param[in] tid		id of owning thread
+ * @param[in] ctxt		application context
+ * @param[in] buf		actual pointer to buffer
+ * @param[in] numElements	number of records
+ * @param[in] v			callback value
+ * @return  A pointer to the buffer to resume filling.
+ */
+VOID * BufferFull(BUFFER_ID id, THREADID tid, const CONTEXT *ctxt, VOID *buf,
+                  UINT64 n, VOID *v)
 {
-    // This is really inefficient - we need a way to record events into a
-    // thread-specific pool before they are shipped to the visualizer.
+    struct BufferData *data = (struct BufferData *)buf;
+
     PIN_GetLock(&theLock, tid);
-    unsigned long long val;
-    val = ((unsigned long long)addr << MV_AddrShift) & MV_AddrMask;
-    val |= type;
-    val |= ((unsigned long long)size << MV_SizeShift) & MV_SizeMask;
-    val |= ((unsigned long long)tid << MV_ThreadShift) & MV_ThreadMask;
-    theBlock->myAddr[theBlock->myEntries] = val;
-    theBlock->myEntries++;
-    if (theBlock->myEntries >= theMaxEntries)
-	flushEvents();
+    for (UINT64 i = 0; i < n; i++)
+    {
+	unsigned long long val;
+	val = ((unsigned long long)data[i].ea << MV_AddrShift) & MV_AddrMask;
+	val |= data[i].type;
+	val |= ((unsigned long long)tid << MV_ThreadShift) & MV_ThreadMask;
+	theBlock->myAddr[theBlock->myEntries] = val;
+	theBlock->myEntries++;
+	if (theBlock->myEntries >= theMaxEntries)
+	    flushEvents();
+    }
     PIN_ReleaseLock(&theLock);
-}
 
-// Print a memory read record
-VOID RecordMemRead(VOID *addr, UINT32 size, THREADID tid)
-{
-    recordEvent(addr, MV_ShiftedRead, size, tid);
-}
-
-// Print a memory write record
-VOID RecordMemWrite(VOID *addr, UINT32 size, THREADID tid)
-{
-    recordEvent(addr, MV_ShiftedWrite, size, tid);
+    return buf;
 }
 
 // Is called for every instruction and instruments reads and writes
@@ -144,27 +156,27 @@ VOID Instruction(INS ins, VOID *v)
     // Iterate over each memory operand of the instruction.
     for (UINT32 memOp = 0; memOp < memOperands; memOp++)
     {
-	bool read = INS_MemoryOperandIsRead(ins, memOp);
 	bool write = INS_MemoryOperandIsWritten(ins, memOp);
+	UINT32 size = INS_MemoryOperandSize(ins, memOp);
 
-	if (write)
-        {
-            INS_InsertPredicatedCall(
-                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
-                IARG_MEMORYOP_EA, memOp,
-		IARG_MEMORYWRITE_SIZE,
-		IARG_THREAD_ID,
-                IARG_END);
-        }
-	else if (read)
-        {
-            INS_InsertPredicatedCall(
-                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
-                IARG_MEMORYOP_EA, memOp,
-		IARG_MEMORYREAD_SIZE,
-		IARG_THREAD_ID,
-                IARG_END);
-        }
+	unsigned long long  type;
+	unsigned long long  datatype;
+
+	if (size <= 1)
+	    datatype = MV_DataChar8;
+	else if (size <= 4)
+	    datatype = MV_DataInt32;
+	else
+	    datatype = MV_DataInt64;
+
+	type = write ? MV_ShiftedWrite : MV_ShiftedRead;
+	type |= ((unsigned long long)size << MV_SizeShift) & MV_SizeMask;
+	type |= datatype << MV_DataShift;
+
+	INS_InsertFillBuffer(ins, IPOINT_BEFORE, theBuffer,
+		     IARG_MEMORYOP_EA, memOp, offsetof(struct BufferData, ea),
+		     IARG_ADDRINT, type, offsetof(struct BufferData, type),
+		     IARG_END);
     }
 }
 
@@ -224,6 +236,18 @@ int main(int argc, char *argv[])
     theBlock = &theSharedData->myData[theBlockIndex];
     theBlock->myEntries = 0;
     
+    // Initialize the memory reference buffer;
+    // set up the callback to process the buffer.
+    //
+    theBuffer = PIN_DefineTraceBuffer(
+	    sizeof(BufferData), NUM_BUF_PAGES, BufferFull, 0);
+
+    if(theBuffer == BUFFER_ID_INVALID)
+    {
+        cerr << "Error: could not allocate initial buffer" << endl;
+        return 1;
+    }
+
     // Register function to be called when the application exits
     PIN_AddFiniFunction(Fini, 0);
 
